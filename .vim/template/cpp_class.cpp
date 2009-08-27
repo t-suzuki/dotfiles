820daa6a09f3c5225ee294fa3fc06bd38e982fa9
tomato <tomato@a679fe3b-1447-42ed-954e-002ad7b0ec2c>
2009-08-27 09:23:17 +0000

    added templates cpp, py
    git-svn-id: file:///var/svn/private/trunk@112 a679fe3b-1447-42ed-954e-002ad7b0ec2c

---
+++ .vim/template/cpp_class.cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+ * \file 
+ * \brief
+ * \author
+ *****************************************************************************/
+#include <iostream>
+
+#include <boost/function.hpp>
+#include <boost/bind.hpp>
+
+class Class {
+public:
+  // methods
+  Class();
+  virtual ~Class();
+
+private:
+  // data members
+};
+
+/* vim: set et sts=2 sw=2 ts=2 fdm=syntax: */
